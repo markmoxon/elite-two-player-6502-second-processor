895f08f3d7921ab928e27dc286b1828962da671c
Mark Moxon <mark@moxon.net>
2026-05-01 14:59:06 +0100

    Add hardcoded lasers, sights, spawning in deep space

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -18193,9 +18193,19 @@ ENDIF
 
  AND #7                 \ Restrict A to a random value in the range 0 to 7
 
- ADC #Y-4               \ Set LASY to four pixels above the centre of the
+                        \ --- Mod: Code removed for Dogfight: ----------------->
+
+\ADC #Y-4               \ Set LASY to four pixels above the centre of the
+\STA LASY               \ screen (#Y), plus our random number, so the laser
+\                       \ dances above and below the centre point
+
+                        \ --- And replaced by: -------------------------------->
+
+ ADC #(Y/2)-4           \ Set LASY to four pixels above the centre of the
  STA LASY               \ screen (#Y), plus our random number, so the laser
                         \ dances above and below the centre point
+
+                        \ --- End of replacement ------------------------------>
 
  JSR DORND              \ Set A and X to random numbers
 
@@ -18261,10 +18271,21 @@ ENDIF
  LDA LASY
  STA Y1
 
- LDA #2*Y-1             \ Set Y2 = 2 * #Y - 1. The constant #Y is 96, the
+                        \ --- Mod: Code removed for Dogfight: ----------------->
+
+\LDA #2*Y-1             \ Set Y2 = 2 * #Y - 1. The constant #Y is 96, the
+\STA Y2                 \ y-coordinate of the mid-point of the space view, so
+\                       \ this sets Y2 to 191, the y-coordinate of the bottom
+\                       \ pixel row of the space view
+
+                        \ --- And replaced by: -------------------------------->
+
+ LDA #Y-1               \ Set Y2 = 2 * #Y - 1. The constant #Y is 96, the
  STA Y2                 \ y-coordinate of the mid-point of the space view, so
                         \ this sets Y2 to 191, the y-coordinate of the bottom
                         \ pixel row of the space view
+
+                        \ --- End of replacement ------------------------------>
 
  JSR LL30               \ Draw a line from (X1, Y1) to (X2, Y2), so that's from
                         \ the centre point to (A, 191)
@@ -18276,8 +18297,17 @@ ENDIF
 
  STY X2                 \ Set X2 = Y
 
- LDA #2*Y-1             \ Set Y2 = 2 * #Y - 1, the y-coordinate of the bottom
+                        \ --- Mod: Code removed for Dogfight: ----------------->
+
+\LDA #2*Y-1             \ Set Y2 = 2 * #Y - 1, the y-coordinate of the bottom
+\STA Y2                 \ pixel row of the space view (as before)
+
+                        \ --- And replaced by: -------------------------------->
+
+ LDA #Y-1               \ Set Y2 = 2 * #Y - 1, the y-coordinate of the bottom
  STA Y2                 \ pixel row of the space view (as before)
+
+                        \ --- End of replacement ------------------------------>
 
  JMP LL30               \ Draw a line from (X1, Y1) to (X2, Y2), so that's from
                         \ the centre point to (Y, 191), and return from
@@ -20325,6 +20355,13 @@ ENDIF
 
  LDX QQ11               \ A >= 152, so we need to check whether this will fit in
                         \ this view, so fetch the view type
+
+                        \ --- Mod: Code added for Dogfight: ------------------->
+
+ BEQ TT87               \ If this is the space view then the y-coordinate is
+                        \ fine, so skip to TT87
+
+                        \ --- End of added code ------------------------------->
 
  BMI TT87               \ If this is the Short-range Chart then the y-coordinate
                         \ is fine, so skip to TT87
@@ -23334,6 +23371,16 @@ ENDIF
 
  JSR TT111              \ Select the system closest to galactic coordinates
                         \ (QQ9, QQ10)
+
+                        \ --- Mod: Code added for Dogfight: ------------------->
+
+ JSR SOLAR              \ Halve our legal status, update the missile indicators,
+                        \ and set up data blocks and slots for the planet and
+                        \ sun
+
+ JMP NLUNCH             \ Jump to NLUNCH to skip the station-spawning code
+
+                        \ --- End of added code ------------------------------->
 
  INC INWK+8             \ Increment z_sign ready for the call to SOS, so the
                         \ planet appears at a z_sign of 1 in front of us when
@@ -43798,12 +43845,32 @@ ENDIF
 
 .SIGHT
 
- LDY VIEW               \ Fetch the laser power for our new view
- LDA LASER,Y
+                        \ --- Mod: Code removed for Dogfight: ----------------->
 
- BEQ LO2                \ If it is zero (i.e. there is no laser fitted to this
-                        \ view), jump to LO2 to return from the subroutine (as
-                        \ LO2 contains an RTS)
+\LDY VIEW               \ Fetch the laser power for our new view
+\LDA LASER,Y
+\
+\BEQ LO2                \ If it is zero (i.e. there is no laser fitted to this
+\                       \ view), jump to LO2 to return from the subroutine (as
+\                       \ LO2 contains an RTS)
+
+                        \ --- And replaced by: -------------------------------->
+
+ LDA #POW               \ Give everyone a pulse laser for now
+
+ LDA playerScreen       \ Draw sights for both players for now
+ PHA
+ STZ playerScreen
+ JSR sigh1
+ SEC
+ ROR playerScreen
+ JSR sigh1
+ PLA
+ RTS
+
+.sigh1
+
+                        \ --- End of replacement ------------------------------>
 
  LDA #YELLOW            \ Send a #SETCOL YELLOW command to the I/O processor to
  JSR DOCOL              \ switch to colour 1, which is yellow in the space view
@@ -44001,6 +44068,18 @@ ENDIF
 
  LDA #Y                 \ Draw a screen-wide horizontal line at pixel row #Y
  JSR NLIN2              \ for the line between the views
+
+ LDA #13                \ Move the text cursor to column 11, row 13
+ JSR DOYC
+ LDA #11
+ JSR DOXC
+
+ LDA VIEW               \ Print player 2's view
+ ORA #&60
+ JSR TT27
+ JSR TT162
+ LDA #175
+ JSR TT27
 
                         \ --- End of added code ------------------------------->
 
@@ -50651,46 +50730,50 @@ ENDIF
 \
 \ ******************************************************************************
 
-.MTIN
+                        \ --- Mod: Code moved for Dogfight: ------------------->
 
- EQUB 16                \ Token  0: a random extended token between 16 and 20
- EQUB 21                \ Token  1: a random extended token between 21 and 25
- EQUB 26                \ Token  2: a random extended token between 26 and 30
- EQUB 31                \ Token  3: a random extended token between 31 and 35
- EQUB 155               \ Token  4: a random extended token between 155 and 159
- EQUB 160               \ Token  5: a random extended token between 160 and 164
- EQUB 46                \ Token  6: a random extended token between 46 and 50
- EQUB 165               \ Token  7: a random extended token between 165 and 169
- EQUB 36                \ Token  8: a random extended token between 36 and 40
- EQUB 41                \ Token  9: a random extended token between 41 and 45
- EQUB 61                \ Token 10: a random extended token between 61 and 65
- EQUB 51                \ Token 11: a random extended token between 51 and 55
- EQUB 56                \ Token 12: a random extended token between 56 and 60
- EQUB 170               \ Token 13: a random extended token between 170 and 174
- EQUB 66                \ Token 14: a random extended token between 66 and 70
- EQUB 71                \ Token 15: a random extended token between 71 and 75
- EQUB 76                \ Token 16: a random extended token between 76 and 80
- EQUB 81                \ Token 17: a random extended token between 81 and 85
- EQUB 86                \ Token 18: a random extended token between 86 and 90
- EQUB 140               \ Token 19: a random extended token between 140 and 144
- EQUB 96                \ Token 20: a random extended token between 96 and 100
- EQUB 101               \ Token 21: a random extended token between 101 and 105
- EQUB 135               \ Token 22: a random extended token between 135 and 139
- EQUB 130               \ Token 23: a random extended token between 130 and 134
- EQUB 91                \ Token 24: a random extended token between 91 and 95
- EQUB 106               \ Token 25: a random extended token between 106 and 110
- EQUB 180               \ Token 26: a random extended token between 180 and 184
- EQUB 185               \ Token 27: a random extended token between 185 and 189
- EQUB 190               \ Token 28: a random extended token between 190 and 194
- EQUB 225               \ Token 29: a random extended token between 225 and 229
- EQUB 230               \ Token 30: a random extended token between 230 and 234
- EQUB 235               \ Token 31: a random extended token between 235 and 239
- EQUB 240               \ Token 32: a random extended token between 240 and 244
- EQUB 245               \ Token 33: a random extended token between 245 and 249
- EQUB 250               \ Token 34: a random extended token between 250 and 254
- EQUB 115               \ Token 35: a random extended token between 115 and 119
- EQUB 120               \ Token 36: a random extended token between 120 and 124
- EQUB 125               \ Token 37: a random extended token between 125 and 129
+\.MTIN
+\
+\EQUB 16                \ Token  0: a random extended token between 16 and 20
+\EQUB 21                \ Token  1: a random extended token between 21 and 25
+\EQUB 26                \ Token  2: a random extended token between 26 and 30
+\EQUB 31                \ Token  3: a random extended token between 31 and 35
+\EQUB 155               \ Token  4: a random extended token between 155 and 159
+\EQUB 160               \ Token  5: a random extended token between 160 and 164
+\EQUB 46                \ Token  6: a random extended token between 46 and 50
+\EQUB 165               \ Token  7: a random extended token between 165 and 169
+\EQUB 36                \ Token  8: a random extended token between 36 and 40
+\EQUB 41                \ Token  9: a random extended token between 41 and 45
+\EQUB 61                \ Token 10: a random extended token between 61 and 65
+\EQUB 51                \ Token 11: a random extended token between 51 and 55
+\EQUB 56                \ Token 12: a random extended token between 56 and 60
+\EQUB 170               \ Token 13: a random extended token between 170 and 174
+\EQUB 66                \ Token 14: a random extended token between 66 and 70
+\EQUB 71                \ Token 15: a random extended token between 71 and 75
+\EQUB 76                \ Token 16: a random extended token between 76 and 80
+\EQUB 81                \ Token 17: a random extended token between 81 and 85
+\EQUB 86                \ Token 18: a random extended token between 86 and 90
+\EQUB 140               \ Token 19: a random extended token between 140 and 144
+\EQUB 96                \ Token 20: a random extended token between 96 and 100
+\EQUB 101               \ Token 21: a random extended token between 101 and 105
+\EQUB 135               \ Token 22: a random extended token between 135 and 139
+\EQUB 130               \ Token 23: a random extended token between 130 and 134
+\EQUB 91                \ Token 24: a random extended token between 91 and 95
+\EQUB 106               \ Token 25: a random extended token between 106 and 110
+\EQUB 180               \ Token 26: a random extended token between 180 and 184
+\EQUB 185               \ Token 27: a random extended token between 185 and 189
+\EQUB 190               \ Token 28: a random extended token between 190 and 194
+\EQUB 225               \ Token 29: a random extended token between 225 and 229
+\EQUB 230               \ Token 30: a random extended token between 230 and 234
+\EQUB 235               \ Token 31: a random extended token between 235 and 239
+\EQUB 240               \ Token 32: a random extended token between 240 and 244
+\EQUB 245               \ Token 33: a random extended token between 245 and 249
+\EQUB 250               \ Token 34: a random extended token between 250 and 254
+\EQUB 115               \ Token 35: a random extended token between 115 and 119
+\EQUB 120               \ Token 36: a random extended token between 120 and 124
+\EQUB 125               \ Token 37: a random extended token between 125 and 129
+
+                        \ --- End of moved code ------------------------------->
 
 \ ******************************************************************************
 \
@@ -54699,6 +54782,72 @@ ENDMACRO
  RTS
 
                         \ --- End of added code ------------------------------->
+
+\ ******************************************************************************
+\
+\       Name: MTIN
+\       Type: Variable
+\   Category: Text
+\    Summary: Lookup table for random tokens in the extended token table (0-37)
+\  Deep dive: Extended text tokens
+\
+\ ------------------------------------------------------------------------------
+\
+\ The ERND token type, which is part of the extended token system, takes an
+\ argument between 0 and 37, and returns a randomly chosen token in the range
+\ specified in this table. This is used to generate the extended description of
+\ each system.
+\
+\ For example, the entry at position 13 in this table (counting from 0) is 66,
+\ so ERND 14 will expand into a random token in the range 66-70, i.e. one of
+\ "JUICE", "BRANDY", "WATER", "BREW" and "GARGLE BLASTERS".
+\
+\ ******************************************************************************
+
+                        \ --- Mod: Code moved for Dogfight: ------------------->
+
+.MTIN
+
+ EQUB 16                \ Token  0: a random extended token between 16 and 20
+ EQUB 21                \ Token  1: a random extended token between 21 and 25
+ EQUB 26                \ Token  2: a random extended token between 26 and 30
+ EQUB 31                \ Token  3: a random extended token between 31 and 35
+ EQUB 155               \ Token  4: a random extended token between 155 and 159
+ EQUB 160               \ Token  5: a random extended token between 160 and 164
+ EQUB 46                \ Token  6: a random extended token between 46 and 50
+ EQUB 165               \ Token  7: a random extended token between 165 and 169
+ EQUB 36                \ Token  8: a random extended token between 36 and 40
+ EQUB 41                \ Token  9: a random extended token between 41 and 45
+ EQUB 61                \ Token 10: a random extended token between 61 and 65
+ EQUB 51                \ Token 11: a random extended token between 51 and 55
+ EQUB 56                \ Token 12: a random extended token between 56 and 60
+ EQUB 170               \ Token 13: a random extended token between 170 and 174
+ EQUB 66                \ Token 14: a random extended token between 66 and 70
+ EQUB 71                \ Token 15: a random extended token between 71 and 75
+ EQUB 76                \ Token 16: a random extended token between 76 and 80
+ EQUB 81                \ Token 17: a random extended token between 81 and 85
+ EQUB 86                \ Token 18: a random extended token between 86 and 90
+ EQUB 140               \ Token 19: a random extended token between 140 and 144
+ EQUB 96                \ Token 20: a random extended token between 96 and 100
+ EQUB 101               \ Token 21: a random extended token between 101 and 105
+ EQUB 135               \ Token 22: a random extended token between 135 and 139
+ EQUB 130               \ Token 23: a random extended token between 130 and 134
+ EQUB 91                \ Token 24: a random extended token between 91 and 95
+ EQUB 106               \ Token 25: a random extended token between 106 and 110
+ EQUB 180               \ Token 26: a random extended token between 180 and 184
+ EQUB 185               \ Token 27: a random extended token between 185 and 189
+ EQUB 190               \ Token 28: a random extended token between 190 and 194
+ EQUB 225               \ Token 29: a random extended token between 225 and 229
+ EQUB 230               \ Token 30: a random extended token between 230 and 234
+ EQUB 235               \ Token 31: a random extended token between 235 and 239
+ EQUB 240               \ Token 32: a random extended token between 240 and 244
+ EQUB 245               \ Token 33: a random extended token between 245 and 249
+ EQUB 250               \ Token 34: a random extended token between 250 and 254
+ EQUB 115               \ Token 35: a random extended token between 115 and 119
+ EQUB 120               \ Token 36: a random extended token between 120 and 124
+ EQUB 125               \ Token 37: a random extended token between 125 and 129
+
+                        \ --- End of moved code ------------------------------->
 
 \ ******************************************************************************
 \
