8a2726e140b59130e8783fab2922b343aeb32b6c
Mark Moxon <mark@moxon.net>
2026-08-12 16:37:58 +0100

    Add "two-player" to the source file headers

--- 1-source-files/main-sources/elite-loader2.asm
+++ 1-source-files/main-sources/elite-loader2.asm
@@ -1,6 +1,6 @@
 \ ******************************************************************************
 \
-\ 6502 SECOND PROCESSOR ELITE I/O LOADER SOURCE (PART 2 OF 2)
+\ TWO-PLAYER 6502 SECOND PROCESSOR ELITE I/O LOADER SOURCE (PART 2 OF 2)
 \
 \ 6502 Second Processor Elite was written by Ian Bell and David Braben and is
 \ copyright Acornsoft 1985
